8456aa995a35706f2bb9bf6f5d8b0fa5dc48ac6c
Michal Stankiewicz <michal.stankiewicz@programisci.eu>
2017-05-09 20:41:20 +0200

    czyszczenie kodu

--- src/pl/si/cw2/Regula.java
+++ src/pl/si/cw2/Regula.java
@@ -1,56 +1,14 @@
 package pl.si.cw2;
-
 import java.util.*;
 
 
-
 public class Regula {
+
     public Map<Integer, String> deskryptor = new HashMap<Integer, String>();
     public String decyzja;
     public int support = 0;
 
 
-    public Regula (String[] obiekt, int[] kombinacja){
-
-        decyzja = obiekt[obiekt.length - 1];
-        for(int nrAtrybutu : kombinacja){
-            String wartoscAtrybutu = obiekt[nrAtrybutu];   // korekta -1 dla coveringa zeby zgadzaly sie indeksy
-            deskryptor.put(nrAtrybutu, wartoscAtrybutu);
-        }
-
-    }
-
-
-    public List<Integer> obiektySupportu(String[][] system)
-    {
-        List<Integer> obiektyIdx = new ArrayList<>();
-        for (int i = 0; i < system.length; i++)
-        {
-            if (czyObiektSpelniaRegule(system[i]))
-                obiektyIdx.add(i);
-        }
-        return obiektyIdx;
-    }
-
-    public boolean czyObiektSpelniaRegule(String[] obiekt){
-
-        for(Map.Entry<Integer, String> deskr : deskryptor.entrySet()){     // foreach dla kazdego deskryptora
-            if(!deskr.getValue().equals(obiekt[deskr.getKey()])){    //   jak nie zgadzaja sie wartosci atrybutow to zwroc false    +  korekta -1 dla coveringu
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    public boolean czyNieSprzeczna(String[][] systemDec) {
-        for (String[] obiekt : systemDec) {
-            if (czyObiektSpelniaRegule(obiekt) && !decyzja.equals(obiekt[obiekt.length - 1]) ) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     public String toString(){
         String out = "";
@@ -68,15 +26,41 @@ public class Regula {
 
 
 
+    public Regula (String[] obiekt, int[] kombinacja){
+
+        decyzja = obiekt[obiekt.length - 1];
+        for(int nrAtrybutu : kombinacja){
+            String wartoscAtrybutu = obiekt[nrAtrybutu];   // korekta -1 dla coveringa zeby zgadzaly sie indeksy
+            deskryptor.put(nrAtrybutu, wartoscAtrybutu);
+        }
+
+    }
 
 
 
+    public List<Integer> obiektySupportu(String[][] system)
+    {
+        List<Integer> obiektyIdx = new ArrayList<>();
+        for (int i = 0; i < system.length; i++)
+        {
+            if (czyObiektSpelniaRegule(system[i]))
+                obiektyIdx.add(i);
+        }
+        return obiektyIdx;
+    }
 
 
 
+    public boolean czyObiektSpelniaRegule(String[] obiekt){
 
+        for(Map.Entry<Integer, String> deskr : deskryptor.entrySet()){     // foreach dla kazdego deskryptora
+            if(!deskr.getValue().equals(obiekt[deskr.getKey()])){    //   jak nie zgadzaja sie wartosci atrybutow to zwroc false    +  korekta -1 dla coveringu
+                return false;
+            }
+        }
+        return true;
+    }
 
-    ///       METODY DLA EXHAUSTIVE
 
 
 
