d1896effe3b8db44d3c184bdaacb9b0a72e91c4a
Michal Stankiewicz <michal.stankiewicz@programisci.eu>
2017-05-09 18:06:42 +0200

    po szypulowemu z przeniesieniem metod do klasy regula

--- src/pl/si/cw2/Regula.java
+++ src/pl/si/cw2/Regula.java
@@ -7,4 +7,65 @@ public class Regula {
     public Map<Integer, String> deskryptor = new HashMap<Integer, String>();
     public String decyzja;
     public int support = 0;
+
+
+    public Regula (String[] obiekt, int[] kombinacja){
+//        Regula r = new Regula();
+        decyzja = obiekt[obiekt.length - 1];
+        for(int nrAtrybutu : kombinacja){
+            String wartoscAtrybutu = obiekt[nrAtrybutu - 1];   // korekta -1 zeby zgadzaly sie indeksy
+            deskryptor.put(nrAtrybutu, wartoscAtrybutu);
+        }
+//        return r;
+    }
+
+
+    public List<Integer> obiektySupportu(String[][] system)
+    {
+        List<Integer> obiektyIdx = new ArrayList<>();
+        for (int i = 0; i < system.length; i++)
+        {
+            if (czyObiektSpelniaRegule(system[i]))
+                obiektyIdx.add(i);
+        }
+        return obiektyIdx;
+    }
+
+    public boolean czyObiektSpelniaRegule(String[] obiekt){
+
+        for(Map.Entry<Integer, String> deskr : deskryptor.entrySet()){     // foreach dla kazdego deskryptora
+            if(!deskr.getValue().equals(obiekt[deskr.getKey()-1])){    //   jak nie zgadzaja sie wartosci atrybutow to zwroc false
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean czyNieSprzeczna(String[][] systemDec) {
+        for (String[] obiekt : systemDec) {
+            if (czyObiektSpelniaRegule(obiekt) && !decyzja.equals(obiekt[obiekt.length - 1]) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String toString(){
+        //System.out.print("\nR" + licznikRegul + ": ");
+        String out = "";
+        for(int key : deskryptor.keySet()){
+            //System.out.print("(a"+key+"="+deskryptor.get(key)+") ");
+            out+="(a"+key+"="+deskryptor.get(key)+") ";
+        }
+        if(support > 1) {
+            //System.out.print("=> d=" + decyzja + " [" + support + "]    Obiekty: ");
+            out+="=> d=" + decyzja + " [" + support + "]    Obiekty: ";
+        }
+        else{
+            //System.out.print("=> d=" + decyzja + "    Obiekty: ");
+            out+="=> d=" + decyzja + "    Obiekty: ";
+        }
+        return out;
+    }
 }
