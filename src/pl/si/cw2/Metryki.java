3c09147040c9f433cd3984a327a51a0ea23339cc
Michal Stankiewicz <michal.stankiewicz@programisci.eu>
2017-05-28 15:41:32 +0200

    dodane metryki (bez perasona)

--- src/pl/si/cw2/Metryki.java
+++ src/pl/si/cw2/Metryki.java
@@ -2,14 +2,22 @@ package pl.si.cw2;
 
 import javafx.util.Pair;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Michal Stankiewicz on 27.05.2017.
  * Programisci.eu
  */
 public class Metryki {
+
+    public static Pair<Double, Integer> manhattan(Integer[] tst, Integer[] trn){
+        Double wynik = 0.0;
+        for(int i=0; i<(tst.length)-1; i++){
+            wynik+=Math.abs((tst[i]-trn[i]));
+        }
+        Pair<Double, Integer> out = new Pair<>(wynik,trn[trn.length-1]);
+        return out;
+    }
 
     public static Pair<Double, Integer> euklidesa(Integer[] tst, Integer[] trn){
         Double wynik = 0.0;
@@ -18,6 +26,40 @@ public class Metryki {
             wynik+=(Math.pow((tst[i]-trn[i]),2));
         }
         Pair<Double, Integer> out = new Pair<>(Math.sqrt(wynik),trn[trn.length-1]);
+        return out;
+    }
+
+    public static Pair<Double, Integer> canberra(Integer[] tst, Integer[] trn){
+        Double wynik = 0.0;
+        for(int i=0; i<(tst.length)-1; i++){
+            wynik+=Math.abs((double)(tst[i]-trn[i])/(tst[i]+trn[i]));
+        }
+        Pair<Double, Integer> out = new Pair<>(wynik,trn[trn.length-1]);
+        return out;
+    }
+
+    public static Pair<Double, Integer> czebyszewa(Integer[] tst, Integer[] trn){
+        List<Integer> listaWynikow = new ArrayList<>();
+        for(int i=0; i<(tst.length)-1; i++){
+            listaWynikow.add(Math.abs((tst[i]-trn[i])));
+        }
+        double wynik = Collections.max(listaWynikow);
+        Pair<Double, Integer> out = new Pair<>(wynik,trn[trn.length-1]);
+        return out;
+    }
+
+    public static Pair<Double, Integer> pearsona(Integer[] tst, Integer[] trn){
+        Double sredniaX = 0.0;
+        Double sredniaY = 0.0;
+        for(int i=0; i<(tst.length)-1; i++){
+            sredniaX+=tst[i];
+            sredniaY+=trn[i];
+        }
+        sredniaX /= (tst.length)-1;
+        sredniaY /= (tst.length)-1;
+
+
+        Pair<Double, Integer> out = new Pair<>(0.0,trn[trn.length-1]);
         return out;
     }
 }
